fe1bbaa37fe45d620745fde5321015576c1436ad
Aiden <aidenjs9@icloud.com>
2024-04-25 10:43:40 -0500

    change up the build process

--- index.js
+++ index.js
@@ -25,12 +25,18 @@ function buildPhase() {
 	return new Promise((resolve, reject) => {
 		console.log('Building!')
 		asar.createPackage(FDE_Settings.BundlePrebuild + ".src",  path.resolve('./plugins/' + FDE_Settings.BundleName)).then(() => {
+			console.log('Created ASAR package.')
 			cpSync(path.resolve('./plugins',FDE_Settings.BundleName), path.resolve('./build/' + FDE_Settings.BundleName))
+			console.log('Copied to plugins folder. Build completed.')
+			if(!FDE_Settings.test) {console.log('No tests to do! Finished.'); return;}
 			exec('npm i',{
 				cwd: path.resolve(FDE_Settings.BundlePrebuild + ".src")
 			}, (err, stdo, stde) => {
+				console.log('Installed dependencies for package for testing')
 				AsarBundleRunner.extract("./plugins/"+FDE_Settings.BundleName).then(bundleName => {
+					console.log('Simulated extraction with ABR')
 					AsarBundleRunner.run(bundleName).then(output => {
+						console.log('Simulated initialization with ABR')
 						FDE_Settings._abr_run_output = output
 						resolve(true)
 					}).catch(reject)
